5efe17cce1238cc730adf48d8347c9511c6caeca
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-12 23:25:03 +0300

    send paran POST request successfully

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,30 +1,28 @@
 const express = require('express'),
   router = express.Router(),
-  UserController = e => {
-    return console.log(e);
-  }
+  UserController = require('../controllers/UserController');
 //require('../controllers/users.controller'),
 const { firsDatabaseInitialization } = require('../database/api')
 //controllers хранит классы, методы которого выступают обработчиками маршрутов
 //services также находятся классы, но их методы отвечают за поставку данных контроллерам
 
-router.use(async (req, res, next) => {
-  let data = false //await UsersService(req)
+// router.use(async (req, res, next) => {
+//   let data = false //await UsersService(req)
 
-  if (data) {
-    req.users = data
-    next()
-  } else
-    return res
-      .status(500)
-      .send({ message: 'Error while getting users' })
-})
+//   if (data) {
+//     req.users = data
+//     next()
+//   } else
+//     return res
+//       .status(500)
+//       .send({ message: 'Error while getting users' })
+// })
 
 router
   .route('/')
-  .get(UserController)
-  .post(UserController)
-  .put(UserController)
-  .delete(UserController)
+  // .get(UserController.getUsers)
+  .post(UserController.getUsers)
+  // .put(UserController.getUsers)
+  // .delete(UserController.getUsers)
 
 module.exports = router
