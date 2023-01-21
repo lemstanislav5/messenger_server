eaf2f7dcc4a485b2de09515b77330fc3adfae171
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-21 17:32:04 +0300

    32323

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 // //!Шифрование Диффи-Хеллмана на JavaScript https://roscenzura.com/threads/1173/
 // //ТУТОРИАЛ https://socket.io/docs/v3/emit-cheatsheet/
 const port = 4000;
-=======
-//!Шифрование Диффи-Хеллмана на JavaScript https://roscenzura.com/threads/1173/
-//ТУТОРИАЛ https://socket.io/docs/v3/emit-cheatsheet/
-const PORT = 4000;
->>>>>>> 6c73bdf64cdfbc756b256b4b1dc8dc1d7805aeb3
 const express = require('express'),
       app = express(),
       http = require('http').Server(app),
@@ -16,7 +10,6 @@ const express = require('express'),
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-<<<<<<< HEAD
 app.use('/api', routes);
 app.use('/index.html', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -25,15 +18,6 @@ app.get('/', (req, res) => {
   res.send('Тест'); 
 });
 
-=======
-// app.use('/api', routes);
-// app.use('/index.html', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-app.get('/', (req, res) => {    
-  res.send('Тест'); 
-});
->>>>>>> 6c73bdf64cdfbc756b256b4b1dc8dc1d7805aeb3
 
 io.on('connection', socket => {
   console.log('A user connected');
@@ -54,12 +38,7 @@ io.on('connection', socket => {
   })
 })
 
-<<<<<<< HEAD
 http.listen(port, () => {
   console.log('listening on *:' + port);
-=======
-http.listen(PORT, function() {
-  console.log('listening on *:' + PORT);
->>>>>>> 6c73bdf64cdfbc756b256b4b1dc8dc1d7805aeb3
 });
 
