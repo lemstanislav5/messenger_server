6c73bdf64cdfbc756b256b4b1dc8dc1d7805aeb3
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-21 13:08:02 +0300

    no use router

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const express = require('express'),
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api', routes);
-app.use('/index.html', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+// app.use('/api', routes);
+// app.use('/index.html', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// })
 app.get('/', (req, res) => {    
   res.send('Тест'); 
 });
