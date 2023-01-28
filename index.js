3e5a5d3a873e383a96103f0278cdd7691c9aff36
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-28 18:37:26 +0300

    add socket/io

--- index.js
+++ index.js
@@ -37,3 +37,22 @@ io.on('connection', socket => {
 http.listen(port, () => {
   console.log('listening on *:' + port);
 });
+
+io.on('connection', socket => {
+  console.log('A user connected');
+//   setTimeout(function() {
+//     //Sending an object when emmiting an event
+//     socket.send('Sent a message 4seconds after connection!');
+//  }, 4000);
+
+  socket.on('disconnect', function () {
+      console.log('A user disconnected');
+  });
+  socket.on('message', message => {
+    console.log(message)
+    console.log(socket.id)
+    socket.send(socket.id, 'Sent a message 4seconds after connection!');
+    socket.emit(`[${socket.id}]: ${message}`)
+    socket.broadcast.emit(`[${socket.id}]: ${message}`)
+  })
+})
