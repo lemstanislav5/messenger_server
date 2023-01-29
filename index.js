64bafeeac496204fae1de40570b640f9e17649f0
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-29 20:54:45 +0300

    jsjsjs

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const {URL, TELEGRAM_API_TOKEN, PASSWORD, PORT} = require('../config.js');
+console.log(URL, TELEGRAM_API_TOKEN, PASSWORD);
+
 const TelegramBot = require('node-telegram-bot-api');
-const TELEGRAM_API_TOKEN = '';
 const bot = new TelegramBot(TELEGRAM_API_TOKEN, {polling: true});
 const localStorage = require('./modules/localStorage')();
-const PASSWORD = '';
 
-const port = 4000;
 const express = require('express'),
       app = express(),
       http = require('http').Server(app),
@@ -17,46 +17,26 @@ app.use(express.static('static'));
 
 app.use('/api', routes);
 app.use('/index.html', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');//!BUILD CLIENT
 })
 app.get('/', (req, res) => {
   res.send('Тест');
 });
 
-
-io.on('connection', socket => {
-  console.log('A user connected');
-  socket.on('disconnect',  () => {
-      console.log('A user disconnected');
-  });
-  socket.on('message', message => {
-    console.log(message)
-    console.log(socket.id)
-    socket.send(socket.id, 'Sent a message 4seconds after connection!');
-    socket.emit(`[${socket.id}]: ${message}`)
-    socket.broadcast.emit(`[${socket.id}]: ${message}`)
-  })
-})
-
-http.listen(port, () => {
-  console.log('listening on *:' + port);
+http.listen(PORT, () => {
+  console.log('listening on *:' + PORT);
 });
 
 io.on('connection', socket => {
   console.log('A user connected');
-//   setTimeout(function() {
-//     //Sending an object when emmiting an event
-//     socket.send('Sent a message 4seconds after connection!');
-//  }, 4000);
-
   socket.on('disconnect', function () {
       console.log('A user disconnected');
   });
   socket.on('message', message => {
     console.log(message)
     console.log(socket.id)
-    socket.send(socket.id, 'Sent a message 4seconds after connection!');
-    socket.emit(`[${socket.id}]: ${message}`)
-    socket.broadcast.emit(`[${socket.id}]: ${message}`)
+    // socket.send(socket.id, 'Sent a message 4seconds after connection!');
+    // socket.emit(`[${socket.id}]: ${message}`)
+    // socket.broadcast.emit(`[${socket.id}]: ${message}`)
   })
 })
