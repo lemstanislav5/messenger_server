0e90556ccbc91dd6ef8db835bdb182b5fcde8487
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-30 11:07:06 +0300

    localStorage

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const {URL, TELEGRAM_API_TOKEN, PASSWORD, PORT} = require('../config.js');
-let SOCKET = null;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(TELEGRAM_API_TOKEN, {polling: true});
 const localStorage = require('./modules/localStorage')();
@@ -7,12 +6,12 @@ const localStorage = require('./modules/localStorage')();
 const dateMessage = () => {
   let date = new Date();
   return date.getDate() +'-'+ date.getMonth() +'-'+ date.getFullYear() +','+ date.getHours()+':'+date.getMinutes();
-} 
+}
 
 //localStorage.getItem(id)
 // firsDatabaseInitialization()
 //     .then(res => {
-      
+
 //     })
 
 const express = require('express'),
@@ -38,30 +37,28 @@ http.listen(PORT, () => {
 });
 
 io.on('connection', socket => {
-  localStorage.setItem('current_visitor_id', socket.id);
-  SOCKET = socket;
   console.log('A user connected');
-})
-
-SOCKET.on('disconnect', function () {
-  console.log('A user disconnected');
-});
-SOCKET.on('message', message => {
-console.log(message)
-console.log(socket.id)
-const chatId = localStorage.getItem('bot_chat_id');
-if(chatId === null) return console.log('Manager offline!')
-//!bot.sendMessage(chatId, 'Received your message'); откуда взять chatId??
-// socket.send(socket.id, 'Sent a message 4seconds after connection!');
-// socket.emit(`[${socket.id}]: ${message}`)
-// socket.broadcast.emit(`[${socket.id}]: ${message}`)
-})
-
-bot.on('message', (message) => {
-  const {chat, date, text} = message;
-  const {first_name, last_name, username}  = chat;
-  localStorage.setItem('bot_chat_id', chat);
-  const id = localStorage.getItem('current_visitor_id');
-  const messages = JSON.stringify({ id, type: 'to', text: message, date: dateMessage(), serverAccepted: false, botAccepted: false });
-  SOCKET.send(id, messages);
+  socket.on('message', message => {
+      localStorage.setItem('current_visitor_id', socket.id);
+      console.log(message)
+      console.log(socket.id)
+      const chatId = localStorage.getItem('bot_chat_id');
+      if(chatId === null) return console.log('Manager offline!')
+      bot.sendMessage(chatId, 'Received your message');
+      // socket.send(socket.id, 'Sent a message 4seconds after connection!');
+      // socket.emit(`[${socket.id}]: ${message}`)
+      // socket.broadcast.emit(`[${socket.id}]: ${message}`)
+  });
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+  bot.on('message', (message) => {
+    const {chat, date, text} = message;
+    const {id, first_name, last_name, username}  = chat;
+    localStorage.setItem('bot_chat_id', chat);
+    const current_visitor_id = localStorage.getItem('current_visitor_id');
+    console.log(id, current_visitor_id);
+    const messages = JSON.stringify({ current_visitor_id, type: 'to', text: text, date: dateMessage(), serverAccepted: false, botAccepted: false });
+    socket.send(id, messages);
+  })
 })
