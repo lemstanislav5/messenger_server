047216c1b19da1586119c2d97822e9d66cce17fd
lemstanislav5 <104723242+lemstanislav5@users.noreply.github.com>
2023-01-30 15:50:57 +0300

    appSocketId

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ const express = require('express'),
       app = express(),
       http = require('http').Server(app),
       io = require('socket.io')(http),
-      routes = require('./routes/index');
+      routes = require('./routes/index'),
+      users = [];
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('static'));
-
-app.use('/api', routes);
-app.use('/index.html', (req, res) => {
-    res.sendFile(__dirname + '/index.html');//!BUILD CLIENT
-})
-app.get('/', (req, res) => res.send('<h2>Тестовый сайт на node js</h2>'));
+   .use(express.urlencoded({ extended: true }))
+   .use(express.static('static'))
+   .use('/api', routes)
+   .use('/index.html', (req, res) => res.sendFile(__dirname + '/index.html'))
+   .get('/', (req, res) => res.send('<h2>Тестовый сайт на node js</h2>'));
 
 http.listen(PORT, () => console.log('listening on *:' + PORT));
 
 io.on('connection', socket => {
+  let user = users.find(item => item.id === socket);
+  if(user === undefined) [...users, {id:'test_hash', name: '', email: ''}];
   console.log('A user connected');
   socket.on('new message', data => {
-    localStorage.setItem('current_visitor_id', socket.id);
+    localStorage.setItem('socketId', socket.id);
     const chatId = localStorage.getItem('bot_chat_id');
     if(chatId === null) return console.log('Manager offline!')
     bot.sendMessage(chatId, data.message);
@@ -36,6 +36,6 @@ bot.on('message', (message) => {
   const {chat, date, text} = message;
   const {id, first_name, last_name, username}  = chat;
   localStorage.setItem('bot_chat_id', id);
-  const socketId = localStorage.getItem('current_visitor_id');
-  io.to(socketId).emit('new message', text);
-})
+  const socketId = localStorage.getItem('socketId');
+  io.to(socketId).emit('new message', socketId + '\n' + text);
+});
