52e23e29094416b5111b2f418aa7feac117f70d7
lemstanislav5 <lemstanislav5@gmail.com>
2023-02-04 23:26:10 +0300

    test111

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const {URL, TELEGRAM_API_TOKEN, PASSWORD, PORT} = require('../config.js');
-console.log(URL, TELEGRAM_API_TOKEN, PASSWORD, PORT);
+let SOCKET = null;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(TELEGRAM_API_TOKEN, {polling: true});
 const localStorage = require('./modules/localStorage')();
+
+const dateMessage = () => {
+  let date = new Date();
+  return date.getDate() +'-'+ date.getMonth() +'-'+ date.getFullYear() +','+ date.getHours()+':'+date.getMinutes();
+} 
+
+//localStorage.getItem(id)
+// firsDatabaseInitialization()
+//     .then(res => {
+      
+//     })
 
 const express = require('express'),
       app = express(),
@@ -10,51 +21,44 @@ const express = require('express'),
       io = require('socket.io')(http),
       routes = require('./routes/index');
 
-let users = [];
-bot.setMyCommands([
-  {command: '/start', description: 'Старт/Меню'},
-  {command: '/visitor list', description: 'Список посетителей'}, //!ЗАПРОСЫ НА ПОЛУЧЕНИЕ СООБЩЕНИЙ
-  {command: '/options', description: 'Настройки'}, //!ЗАПРОСЫ НА ПОЛУЧЕНИЕ СООБЩЕНИЙ
-]);
 app.use(express.json())
-   .use(express.urlencoded({ extended: true }))
-   .use(express.static('static'))
-   .use('/api', routes); //!ЗАПРОСЫ НА ПОЛУЧЕНИЕ СООБЩЕНИЙ
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('static'));
+
+app.use('/api', routes);
+app.use('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/index.html');//!BUILD CLIENT
+})
+app.get('/', (req, res) => {
+  res.send('Тест');
+});
 
 http.listen(PORT, () => {
   console.log('listening on *:' + PORT);
 });
 
 io.on('connection', socket => {
-  //Ищем пользователя по socketId в массиве users
-  let user = users.find(item => item.socketId === socket.id);
-  // Добавить пользователя в массив
-  if(user === undefined) users.push({socketId: socket.id, name: '', email: ''});
+  SOCKET = socket;
   console.log('A user connected');
-  socket.on('new message', data => {
-    //Ищем пользователя по socketId в массиве users
-    let user = users.find(item => item.socketId === socket.id);
-    //Если пользователь предтставился указываем его имя, в противном случае идентификатор сокета
-    let name = (user === undefined && user.name !== undefined && user.name !== '') ? user.name : 'user unknown\n[socketId' + socket.id + ']';
-    localStorage.setItem('socketId', socket.id);
-    const chatId = localStorage.getItem('bot_chat_id');
-    if(chatId === null) return console.log('Manager offline!')
-    bot.sendMessage(chatId, name + '\n' + data.message);
-  });
-  socket.on('disconnect', () => {
-    //Ищем пользователя по socketId в массиве users
-    let user = users.find(item => item.socketId === socket.id);
-    //Определям индекс пользователя
-    let index = users.indexOf(user);
-    //Удаляем пользователя из массива
-    users.splice(1, index);
-    console.log('A user disconnected')
-  });
+})
+
+SOCKET.on('disconnect', function () {
+  console.log('A user disconnected');
 });
+SOCKET.on('message', message => {
+localStorage.setItem('current_visitor_id', socket.id);
+console.log(message)
+console.log(socket.id)
+//!bot.sendMessage(chatId, 'Received your message'); откуда взять chatId??
+// socket.send(socket.id, 'Sent a message 4seconds after connection!');
+// socket.emit(`[${socket.id}]: ${message}`)
+// socket.broadcast.emit(`[${socket.id}]: ${message}`)
+})
+
 bot.on('message', (message) => {
   const {chat, date, text} = message;
-  const {id, first_name, last_name, username}  = chat;
-  localStorage.setItem('bot_chat_id', id);
-  const socketId = localStorage.getItem('socketId');
-  io.to(socketId).emit('new message', text);
-});
+  const {first_name, last_name, username}  = chat;
+  const id = localStorage.setItem('current_visitor_id');
+  const messages = JSON.stringify({ id, type: 'to', text: message, date: dateMessage(), serverAccepted: false, botAccepted: false });
+  SOCKET.send(id, messages);
+})
