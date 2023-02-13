9cab06fb9913107fe0dbfce49e2773081708ac23
lemstanislav5 <lemstanislav5@gmail.com>
2023-02-13 07:15:13 +0300

    'fix'

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -29,26 +29,24 @@ class UsersController {
       console.log('Сокет не нуждается в обновлении.');
     }
   }
-  async setCurrent(chatId, required){
+  async setCurrent(chatId, required) {
     const users = await getCurrentUser();
-    console.log('users', users)
-    console.log(users.length, required)
     if(users.length === 0 || required === 1) {
       await setCurrentUser(chatId);
       console.log('Текущим пользователем выбран: ' + chatId);
     } 
   }
-  async delCurrent(chatId){
+  async delCurrent(chatId) {
     await delCurrentUser(chatId);
     console.log('Статус "текущий" у пользователя ' + chatId + ' удален в разъединением связи!');
   }
-  getCurrent(){
+  getCurrent() {
+    console.log('Получаем текущего пользователя');
     return getCurrentUser();
   }
-  async getSocketCurrentUser(chatId){
-    console.log('Получаем пользовтаеля!');
+  async getSocketCurrentUser(chatId) {
+    console.log('Получаем socketId текущего пользовтаеля!');
     const user = await findUser(chatId);
-    console.log('getSocketCurrentUser', user);
     if (user.length === 0) return false;
     return user[0].socketId;
   }
