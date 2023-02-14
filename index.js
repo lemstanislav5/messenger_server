d00a65022675ea935a20ad70d8a259810235261c
lemstanislav5 <lemstanislav5@gmail.com>
2023-02-14 10:15:02 +0300

    fix

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ io.on('connection', socket => {
     UsersController.setCurrent(chatId);
     // В зависимости от результата поиска добовляем или обновляем socketId
     UsersController.addOrUpdateUser(socket, chatId);
-    MessegesController.add(chatId, socket.id, id, text, new Date().getTime(), 'from', read = 0); 
+    MessegesController.add(chatId, socket.id, id, text, new Date().getTime(), 'from', read = 0);
     const manager = await ManagerController.get(id);
-    console.log('UsersController.get', manager)
+    console.log('UsersController.get', manager);
     // Сообщаем пользователю об отсутствии менеджера
-    if (manager.length === 0 || manager[0].accest === 0) 
+    if (manager.length === 0 || manager[0].accest === 0)
       return io.to(socket.id).emit('new message', 'Менеджер offline!');
     //! Передаем сообщение боту read = 1
-    MessegesController.sendMessegesToBot(bot, io, text, chatId, socket); 
+    MessegesController.sendMessegesToBot(bot, io, text, chatId, socket);
 
   });
   socket.on('disconnect', () => {
@@ -65,25 +65,25 @@ bot.on('message', async (message) => {
         if (currentUser.length === 0) {
           return MessegesController.sendBotNotification(bot, id, 'Адресат вашего сообщения не выбран!');
         } else {
-          console.log(currentUser);
+          console.log('currentUser', currentUser);
           const socketId = await UsersController.getSocketCurrentUser(currentUser[0].chatId);
-          console.log(socketId);
+          console.log('socketId', socketId);
           if (!socketId) return MessegesController.sendBotNotification(bot, id, 'Адресат не найден в базе!');
           io.to(socketId).emit('new message', text);
-          //! Проверка доставки сообщения 
+          //! Проверка доставки сообщения
           let idMessage = 9999999999 - Math.round(0 - 0.5 + Math.random() * (8999999999 - 0 + 1));
           MessegesController.add(id, socketId, idMessage, text, new Date().getTime(), 'to', read = 0);
         }
       }
     }
-  
+
   }
 });
 
 bot.on('callback_query', async msg => {
   const chatId = msg.data;
   //! При выводе сообщений подьзователя обновляю данные сообщения как прочитанные
-  //! MessegesController.add(chatId, socket.id, id, text, new Date().getTime(), 'from', delivered = 1, read = 0); 
+  //! MessegesController.add(chatId, socket.id, id, text, new Date().getTime(), 'from', delivered = 1, read = 0);
   UsersController.setCurrent(chatId, 1);
 });
 //! bot.sendPhoto(msg.chat.id,"https://www.somesite.com/image.jpg" );
