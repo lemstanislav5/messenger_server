6360092c8c7573b347278372211a4fff42b2bb6f
lemstanislav5 <lemstanislav5@gmail.com>
2023-03-02 18:33:03 +0300

    82221222

--- index.js
+++ index.js
@@ -84,9 +84,9 @@ io.on('connection', socket => {
     } else if (type === 'pdf' || type === 'doc' || type === 'docx' || type === 'txt') {
       section =  'documents';
     } else if (type === 'mp3' || type === 'mpeg') {
-      dir = 'audio';
+      section = 'audio';
     } else if (type === 'mp4' || type ===  'wav') {
-      dir = 'video';
+      section = 'video';
     }
     let dir = __dirname + '/media/' + section;
     console.log(dir);
