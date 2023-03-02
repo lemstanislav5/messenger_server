ca7a3292c0c910f4082eda0997e48bcf70bcfe73
lemstanislav5 <lemstanislav5@gmail.com>
2023-03-02 16:47:27 +0300

    876134781672367892212112

--- controllers/MessegesController.js
+++ controllers/MessegesController.js
@@ -31,6 +31,7 @@ class MessegesController {
   }
 
   async sendFile(bot, pathFile, section, callback) {
+    cosnole.log(section)
     const manager = await getIdManager();
     let send;
     if (section === 'images') {
