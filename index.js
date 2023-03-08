a4cb2c52a1f67f6c61b3cc7806daf13f291d7554
lemstanislav5 <lemstanislav5@gmail.com>
2023-03-08 19:31:58 +0300

    213123123

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/media*', (req, res) => {
     } else {
       return res
         .status(200)
-        .send({ data: req.users[req.query.id] });
+        .send({ ok: false });
     }
   } catch(err) {
     console.error(err);
