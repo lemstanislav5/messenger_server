849147d5ef7a0e78ed4ea22fdf533f9800e84638
lemstanislav5 <lemstanislav5@gmail.com>
2023-03-08 12:25:43 +0300

    234234234234

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const process = require('process');
 const fs = require("fs");
-const cors = require('cors')
 const {URL, PASSWORD, PORT} = require('../config.js');
 const bot = require('./services/telegramBot');
 bot.setMyCommands([ { command: '/start', description: 'Старт(меню)' }]);
@@ -17,7 +16,14 @@ const express = require('express'),
       http = require('http').Server(app),
       io = require('socket.io')(http, { maxHttpBufferSize: 1e8, pingTimeout: 60000 });
 
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use('/media/images/', express.static(__dirname + '/media/images/'));
 app.use('/media/documents/', express.static(__dirname + '/media/documents/'));
 app.use('/media/audio/', express.static(__dirname + '/media/audio/'));
